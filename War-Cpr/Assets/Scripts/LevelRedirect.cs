5b8c62e30835e9a1854ca9c66e548ff120ac724f
Arafa42 <s107661@ap.be>
2020-11-11 16:19:00 +0100

    loading screen added + main menu navigation

---
+++ War-Cpr/Assets/Scripts/LevelRedirect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelRedirect : MonoBehaviour
+{
+
+    public static int redirectToLevel;
+    public static int nextLevel;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(redirectToLevel == 2)
+        {
+            SceneManager.LoadScene(redirectToLevel);
+        }
+
+
+    }
+}
