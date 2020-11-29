5948473b98189a3ac6514d289c55ff2882a20c04
Arafa42 <s107661@ap.be>
2020-11-29 17:09:50 +0100

    menu + splash + intro completed

---
+++ War-Cpr/Assets/Scripts/IntroButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroButton : MonoBehaviour
+{
+    public AudioSource buttonPress;
+
+    public void Confirm()
+    {
+
+        buttonPress.Play();
+        LevelRedirect.redirectToLevel = 2;
+        SceneManager.LoadScene(2);
+    }
+}
