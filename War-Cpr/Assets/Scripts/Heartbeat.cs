3c4c49990c83b449ffd5de4eac179843486d00e4
Arafa42 <s107661@ap.be>
2020-12-09 04:02:10 +0100

    effect updates

--- War-Cpr/Assets/Scripts/Heartbeat.cs
+++ War-Cpr/Assets/Scripts/Heartbeat.cs
@@ -6,12 +6,14 @@ public class Heartbeat : MonoBehaviour
 {
     public AudioSource heartSound;
     public AudioSource warSound;
-
+    public AudioSource stressSounds;
 
     void Start()
     {
         StartCoroutine(SoundDelay());
         StartCoroutine(WarSoundsDelay());
+        StartCoroutine(StressSoundsDelay());
+
 
     }
 
@@ -23,7 +25,12 @@ public class Heartbeat : MonoBehaviour
 
     IEnumerator WarSoundsDelay()
     {
-        yield return new WaitForSeconds(40f);
+        yield return new WaitForSeconds(42f);
         warSound.Play();
+    }
+    IEnumerator StressSoundsDelay()
+    {
+        yield return new WaitForSeconds(38f);
+        stressSounds.Play();
     }
 }
