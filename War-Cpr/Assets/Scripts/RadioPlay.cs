3a6a404fc506f8a09cea98a8f6bec467d3850200
Arafa42 <s107661@ap.be>
2020-12-09 01:35:05 +0100

    radio script added

---
+++ War-Cpr/Assets/Scripts/RadioPlay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioPlay : MonoBehaviour
+{
+    public AudioSource radioSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SoundDelay());
+    }
+
+
+    IEnumerator SoundDelay()
+    {
+        yield return new WaitForSeconds(38f);
+        radioSound.Play();
+    }
+
+}
