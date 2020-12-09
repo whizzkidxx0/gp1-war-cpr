2a5334c011861d01e0ec3f91dad92e8d7bce04bb
Arafa42 <s107661@ap.be>
2020-12-09 03:36:27 +0100

    multiple sound and visual effects added

---
+++ War-Cpr/Assets/Scripts/Heartbeat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heartbeat : MonoBehaviour
+{
+    public AudioSource heartSound;
+    public AudioSource warSound;
+
+
+    void Start()
+    {
+        StartCoroutine(SoundDelay());
+        StartCoroutine(WarSoundsDelay());
+
+    }
+
+    IEnumerator SoundDelay()
+    {
+        yield return new WaitForSeconds(42f);
+        heartSound.Play();
+    }
+
+    IEnumerator WarSoundsDelay()
+    {
+        yield return new WaitForSeconds(40f);
+        warSound.Play();
+    }
+}
--- War-Cpr/Assets/Scripts/RadioPlay.cs
+++ War-Cpr/Assets/Scripts/RadioPlay.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class RadioPlay : MonoBehaviour
 {
     public AudioSource radioSound;
+    public AudioSource alarmSound;
+
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SoundDelay());
+        StartCoroutine(AlarmDelay());
+
     }
 
 
     IEnumerator SoundDelay()
     {
-        yield return new WaitForSeconds(38f);
+        yield return new WaitForSeconds(35f);
         radioSound.Play();
+    }
+
+    IEnumerator AlarmDelay()
+    {
+        yield return new WaitForSeconds(40f);
+        alarmSound.Play();
     }
 
 }
