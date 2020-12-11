44d507e71e0e57c814acf0627f96e3b1216f47f5
Arafa42 <s107661@ap.be>
2020-12-11 03:37:05 +0100

    animation transition script added

---
+++ War-Cpr/Assets/Scripts/AnimationSoldier.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSoldier : MonoBehaviour
+{
+
+    public GameObject soldier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AnimationStart());
+    }
+
+
+
+    IEnumerator AnimationStart()
+    {
+        yield return new WaitForSeconds(45);
+        soldier.GetComponent<Animation>().Play("soldierSittin2");
+    }
+
+}
