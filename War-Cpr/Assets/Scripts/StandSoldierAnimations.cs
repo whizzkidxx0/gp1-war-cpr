e19ec1a7af0008f643924b57282e8f2914374fac
Arafa42 <s107661@ap.be>
2020-12-13 01:30:19 +0100

    soldier running to counter animation

---
+++ War-Cpr/Assets/Scripts/StandSoldierAnimations.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandSoldierAnimations : MonoBehaviour
+{
+    public GameObject soldier;
+    private Animation anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AnimationStart());
+        StartCoroutine(RunStart());
+
+    }
+
+
+
+    IEnumerator AnimationStart()
+    {
+        yield return new WaitForSeconds(0);
+        soldier.GetComponent<Animation>().Play("soldierTalking");
+    }
+
+
+    IEnumerator RunStart()
+    {
+        yield return new WaitForSeconds(47);
+        soldier.GetComponent<Animation>().Play("runToCounter");
+        yield return new WaitForSeconds(1.5f);
+        soldier.GetComponent<Animation>().Play("runToCounter2");
+        
+
+        //soldier.GetComponent<Animation>().Play("soldierSittin3");
+    }
+
+}
