ddbdaa0e4f8790255a8b070a9e3d1bba611ffffa
Arafa42 <s107661@ap.be>
2020-12-13 03:21:12 +0100

    explosion script and sound effect added

---
+++ War-Cpr/Assets/Scripts/Explosion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public AudioSource explosionSoundEffects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ExplosionPlayScript());
+    }
+
+
+    IEnumerator ExplosionPlayScript()
+    {
+        yield return new WaitForSeconds(52);
+        explosionSoundEffects.Play();
+    }
+}
