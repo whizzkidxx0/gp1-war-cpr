76fb5ce0d5c8854037f22ea27ed222abb272705f
Arafa42 <s107661@ap.be>
2020-12-13 19:40:48 +0100

    scene transition to cpr added

---
+++ War-Cpr/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SceneChange());
+    }
+
+    IEnumerator SceneChange()
+    {
+        yield return new WaitForSeconds(74);
+        LevelRedirect.redirectToLevel = 2;
+        SceneManager.LoadScene(8);
+    }
+
+}
