dcd2cf83db97161e24e0d5cd5b320df1980a30c2
Arafa42 <s107661@ap.be>
2020-12-13 17:23:48 +0100

    camera shake script added + some bug fixes

---
+++ War-Cpr/Assets/Scripts/CameraShakeTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakeTrigger : MonoBehaviour
+{
+
+    public CameraShake cameraShake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(camShake());
+    }
+
+
+   IEnumerator camShake()
+    {
+        yield return new WaitForSeconds(62);
+        StartCoroutine(cameraShake.Shake(2.5f, .7f));
+    }
+}
