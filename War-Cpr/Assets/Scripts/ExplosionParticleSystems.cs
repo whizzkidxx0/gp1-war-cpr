eaad48484930d6b563f59b562a93732c410dabdd
Arafa42 <s107661@ap.be>
2020-12-15 02:43:17 +0100

    scene one feedback modifications

--- War-Cpr/Assets/Scripts/ExplosionParticleSystems.cs
+++ War-Cpr/Assets/Scripts/ExplosionParticleSystems.cs
@@ -19,7 +19,18 @@ public class ExplosionParticleSystems : MonoBehaviour
     public GameObject explosionParticle13;
     public GameObject explosionParticle14;
     public GameObject explosionParticle15;
-  
+    public GameObject explosionParticle16;
+    public GameObject explosionParticle17;
+    public GameObject explosionParticle18;
+    public GameObject explosionParticle19;
+    public GameObject explosionParticle20;
+    public GameObject explosionParticle21;
+    public GameObject explosionParticle22;
+    public GameObject explosionParticle23;
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -47,6 +58,16 @@ public class ExplosionParticleSystems : MonoBehaviour
         explosionParticle13.SetActive(true);
         explosionParticle14.SetActive(true);
         explosionParticle15.SetActive(true);
+        explosionParticle16.SetActive(true);
+        explosionParticle17.SetActive(true);
+        explosionParticle18.SetActive(true);
+        explosionParticle19.SetActive(true);
+        explosionParticle20.SetActive(true);
+        explosionParticle21.SetActive(true);
+        explosionParticle22.SetActive(true);
+        explosionParticle23.SetActive(true);
+
+
 
     }
 
