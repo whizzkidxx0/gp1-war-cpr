83381b22c80fba4e57f966dbb34098876ce35530
Arafa42 <s107661@ap.be>
2020-12-15 18:06:14 +0100

    start scene broken bunker optimized

---
+++ War-Cpr/Assets/Scripts/VolumeHeartBeat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeHeartBeat : MonoBehaviour
+{
+    public AudioSource heartbeat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DimVolume());
+    }
+
+    IEnumerator DimVolume()
+    {
+        yield return new WaitForSeconds(4f);
+        heartbeat.volume = .2f;
+        yield return new WaitForSeconds(2f);
+        heartbeat.volume = .09f;
+
+    }
+}
