0bd30c9e3afc7b6b5f25785f1e55a956d5ad7e6c
Arafa42 <s107661@ap.be>
2020-12-28 00:16:44 +0100

    wake up script added for animation and sound handling

---
+++ War-Cpr/Assets/Scripts/PlayWakeUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayWakeUp : MonoBehaviour
+{
+
+    public AudioSource playWakeUp;
+    public AudioSource stopHeartbeat;
+    public GameObject injuredSoldier;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayWakeUpSoundAndAnimation());
+        StartCoroutine(StopHeartBeat());
+
+    }
+
+
+    IEnumerator PlayWakeUpSoundAndAnimation()
+    {
+        yield return new WaitForSeconds(3f);
+        playWakeUp.Play();
+        injuredSoldier.GetComponent<Animation>().Play("brokenBunker");
+    }
+
+    IEnumerator StopHeartBeat()
+    {
+        yield return new WaitForSeconds(7f);
+        stopHeartbeat.Stop();
+
+    }
+
+}
