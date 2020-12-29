a863238933fc4894b217be8ad6c0bd228e8146a3
Arafa42 <s107661@ap.be>
2020-12-29 13:57:23 +0100

    nextscene start added to script

--- War-Cpr/Assets/Scripts/Test.cs
+++ War-Cpr/Assets/Scripts/Test.cs
@@ -10,14 +10,24 @@ public class Test : MonoBehaviour
     public Text text;
     private float seconds;
     public GameObject hand;
+    public GameObject txt;
+    public static bool playHandAnim = false;
     private Animation anim;
     public static bool startTimer = false;
+    public static bool nextStep = false;
+    public GameObject cube;
+    public GameObject canvas;
+    public AudioSource playNext;
+    int counter = 0;
     // Start is called before the first frame update
     public void PlayAnim()
     {
         startTimer = true;
-        hand.SetActive(false);
-        hand.GetComponent<Animation>().Stop("NewHandAnim");
+        if (playHandAnim == false)
+        {
+            hand.SetActive(false);
+            hand.GetComponent<Animation>().Stop("NewHandAnim");
+        }
     }
 
 
@@ -26,18 +36,51 @@ public class Test : MonoBehaviour
     {
         if (startTimer)
         {
+                txt.SetActive(true);
                 totalTime -= Time.deltaTime;
                 seconds = (int)(totalTime % 60);
-               
-                if (seconds <= 0)
+
+
+            if (seconds <= 0)
                 {
+                    cube.SetActive(false);
+                    canvas.SetActive(false);
                     text.text = "NEXT STEP";
+                    nextStep = true;
+                    hand.GetComponent<Animation>().Stop("NewHandAnim");
+                    hand.SetActive(false);
+                    startTimer = false;
                 }
                 else
                 {
                 text.text = seconds.ToString();
+                playHandAnim = true;
                 }
          
         }
+
+
+        if (nextStep)
+        {
+            if (playNext.isPlaying == false)
+            {
+                StartCoroutine(playNextScene()); 
+            }
+        }
     }
+
+
+
+    IEnumerator playNextScene()
+    {
+        playNext.Play();
+        yield return new WaitForSeconds(3);
+        playNext.Stop();
+        totalTime = 30;
+        nextStep = false;
+        yield return new WaitForSeconds(3);
+        //startTimer = true;
+    }
+
+
 }
