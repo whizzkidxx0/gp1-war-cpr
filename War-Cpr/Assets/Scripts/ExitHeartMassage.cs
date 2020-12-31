4bd3ffc16b6f9fb2cc6de278b36bee0854063a6d
Arafa42 <s107661@ap.be>
2020-12-31 02:04:59 +0100

    onheartmassagee exit script added

---
+++ War-Cpr/Assets/Scripts/ExitHeartMassage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ExitHeartMassage : MonoBehaviour
+{
+    public GameObject hand;
+    public GameObject timerText;
+    public Text counterText;
+
+    public void ExitMassage()
+    {
+        HeartMassage.cubeParentInteract = false;
+        StartCoroutine(AnimationStart());
+
+
+        if (!HeartMassage.cubeParentInteract)
+        {
+            ChildCubeInterract.counter = 0;
+            counterText.text = ChildCubeInterract.counter.ToString();
+        }
+
+    }
+
+    IEnumerator AnimationStart()
+    {
+        if (HeartMassage.nextStepHeartMassage == false)
+        {
+            HeartMassage.startTimerHeartMassage = false;
+
+            if (HeartMassage.startTimerHeartMassage == false)
+            {
+                timerText.SetActive(false);
+            }
+
+            HeartMassage.totalTimeHeartMassage = 18;
+            yield return new WaitForSeconds(.2f);
+
+            if (HeartMassage.playHandAnimHeartMassage)
+            {
+                hand.SetActive(true);
+                hand.GetComponent<Animation>().Play("cprHintAnim");
+            }
+        }
+    }
+}
