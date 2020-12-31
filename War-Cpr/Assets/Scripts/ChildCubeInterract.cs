91546b70d080b845f4d9c50cc3595024de862092
Arafa42 <s107661@ap.be>
2020-12-31 02:06:18 +0100

    childcube script added

---
+++ War-Cpr/Assets/Scripts/ChildCubeInterract.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChildCubeInterract : MonoBehaviour
+{
+    public static bool childCubeInterract = false;
+    public static int counter = 0;
+    public Text counterText;
+    public GameObject txt;
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        //txt.SetActive(true);
+        childCubeInterract = true;
+        if(childCubeInterract && HeartMassage.cubeParentInteract)
+        {
+            counter++;
+            counterText.text = counter.ToString();
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
