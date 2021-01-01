be2bf1afc6c3c41ca8728ce50635fb16c137a16b
Arafa42 <s107661@ap.be>
2021-01-01 23:47:24 +0100

    child cube script updates

--- War-Cpr/Assets/Scripts/ChildCubeInterract.cs
+++ War-Cpr/Assets/Scripts/ChildCubeInterract.cs
@@ -7,7 +7,9 @@ public class ChildCubeInterract : MonoBehaviour
 {
     public static bool childCubeInterract = false;
     public static int counter = 0;
+    public static int counter2 = 0;
     public Text counterText;
+    public Text counterText2;
     public GameObject txt;
 
 
@@ -18,8 +20,18 @@ public class ChildCubeInterract : MonoBehaviour
         childCubeInterract = true;
         if(childCubeInterract && HeartMassage.cubeParentInteract)
         {
-            counter++;
-            counterText.text = counter.ToString();
+            if (HeartMassage.sceneCounter == 1)
+            {
+                counter++;
+                counterText.text = counter.ToString();
+                PlayerPrefs.SetInt("SecondScore", counter);
+
+            }
+            else if(HeartMassage.sceneCounter == 0){
+                counter2++;
+                counterText2.text = counter2.ToString();
+                PlayerPrefs.SetInt("FirstScore", counter2);
+            }
         } 
     }
 
