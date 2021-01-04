313adf6298f74689b1154d787e31e34c998fb633
Arafa42 <s107661@ap.be>
2021-01-04 23:00:05 +0100

    visionfacde script added

---
+++ War-Cpr/Assets/Scripts/VisionFade.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class VisionFade : MonoBehaviour
+{
+    public GameObject volObj;
+    public Volume v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WakeAndBake());
+    }
+
+
+    IEnumerator WakeAndBake()
+    {
+        yield return new WaitForSeconds(1);
+        volObj.SetActive(true);
+        for (int i = 0; i < 3000; i++)
+        {
+            v.weight = .5f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .6f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .7f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .8f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .9f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = 1f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .9f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .8f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .7f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .6f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .5f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .4f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .5f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .6f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .7f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .8f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .9f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = 1f;
+            yield return new WaitForSeconds(0.1f);
+            v.weight = .9f;
+            yield return new WaitForSeconds(0.1f);
+
+        }
+
+        v.weight = .8f;
+        yield return new WaitForSeconds(0.1f);
+        v.weight = .7f;
+        yield return new WaitForSeconds(0.1f);
+
+    }
+}
