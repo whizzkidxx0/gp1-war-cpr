a9941baa211e5a50dc49151a0176e4c2557a0040
Arafa42 <s107661@ap.be>
2021-01-05 02:02:39 +0100

    finalscore with playerprefences addedy to final scene

---
+++ War-Cpr/Assets/Scripts/FinalScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FinalScore : MonoBehaviour
+{
+
+    public Text text;
+    public GameObject cvs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "SCORE : " + PlayerPrefs.GetFloat("FinalScore").ToString()+"%";
+        StartCoroutine(FadeSceneTransition());
+    }
+
+
+
+    IEnumerator FadeSceneTransition()
+    {
+        yield return new WaitForSeconds(5);
+        cvs.SetActive(true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(1);
+    }
+
+}
