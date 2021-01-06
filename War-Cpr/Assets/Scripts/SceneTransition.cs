96d95ed93d516fdb6b6920de7702cbd763ad86c3
Arafa42 <s107661@ap.be>
2021-01-06 21:11:13 +0100

    scene transition init static vars

--- War-Cpr/Assets/Scripts/SceneTransition.cs
+++ War-Cpr/Assets/Scripts/SceneTransition.cs
@@ -8,6 +8,22 @@ public class SceneTransition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        HeartMassage.playHandAnimHeartMassage = false;
+        HeartMassage.nextStepHeartMassage = false;
+        HeartMassage.totalTimeHeartMassage = 18;
+        HeartMassage.startTimerHeartMassage = false;
+        HeartMassage.cubeParentInteract = false;
+        HeartMassage.sceneCounter = 0;
+        Test.totalTime = 5;
+        Test.playHandAnim = false;
+        Test.startTimer = false;
+        Test.nextStep = false;
+        ChildCubeInterract.childCubeInterract = false;
+        ChildCubeInterract.counter = 0;
+        ChildCubeInterract.counter2 = 0;
+        PlayerPrefs.SetFloat("FinalScore", 0);
+        PlayerPrefs.SetInt("SecondScore", 0);
+
         StartCoroutine(SceneChange());
     }
 
