a3443f4957e7b50c3cf29da2aea442a6bf523334
Arafa42 <s107661@ap.be>
2021-01-06 21:10:31 +0100

    new camerashake script added to second scene

---
+++ War-Cpr/Assets/Scripts/CameraShakeTrigger2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakeTrigger2 : MonoBehaviour
+{
+    public CameraShake cameraShake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(camShake());
+    }
+
+
+    IEnumerator camShake()
+    {
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(cameraShake.Shake(2.5f, .7f));
+    }
+}
